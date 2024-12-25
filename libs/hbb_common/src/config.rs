3066a9dadc3e8829e3e0bf0325c6c67653dbb354
zhanglei1371 <zhanglei1371@126.com>
2024-12-25 16:03:14 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.officexlbg.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["101.35.54.140"];
+pub const PUBLIC_RS_PUB_KEY: &str = "5pXEX3qSEPgtmHamq1OYT+LbowS3HaFcJ5ae3lha6kk=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
